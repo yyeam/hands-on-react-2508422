3a9faca8b75ebdd3f17031907b8dfc75a44f2600
yyeam <122849493+yyeam@users.noreply.github.com>
2023-01-18 06:36:47 +0000

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,14 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
+          
           <Welcome name="StarGazers" />
+
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          
           <Support />
+
         </hgroup>
       </article>
     </div>
--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,7 +1,15 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
+const pageTitle = document.title;
 
 export default () => {
   const [count, setCount] = useState(0)
+
+  useEffect(()=>{
+    // if(count > 0){
+    //   document.title = `${pageTitle}--${count}`;
+    // }
+    count && (document.title = `${pageTitle}--${count}`)
+  })
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? "Click to support": `Supported ${count} times`}
